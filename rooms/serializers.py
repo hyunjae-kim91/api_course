d5c4b4f2352647cc44c93e3d3fff35f639edc702
KIM_HYUNJAE04 <khyunjae10@gmail.com>
2021-08-30 15:25:29 +0900

    second commit

---
+++ rooms/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Room
+from users.serializers import UserSerializer
+
+
+class RoomSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer()
+
+    is_fav = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
+        exclude = ("modified",)
+        read_only_fields = ("user", "id", "created", "updated")
+
+    def validate(self, data):
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+        else:
+            check_in = data.get("check_in")
+            check_out = data.get("check_out")
+            if check_in == check_out:
+                raise serializers.ValidationError("Not enough time between changes")
+        return data
+
+    def get_is_fav(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
